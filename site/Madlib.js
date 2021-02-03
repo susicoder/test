ab9096940b0a1281407ac3705ab05f7d82ea5dbd
Susithra <tharasusicse02@gmail.com>
2021-02-03 08:55:13 +0530

    First Java script

---
+++ site/Madlib.js
@@ -0,0 +1,7 @@
+function wordBlanks(myNoun,myAdjective,myVerb,myAdverb)
+{
+	var result="";
+	return result;
+}
+result+= "The"+myAdjective+""+myNoun+""+myVerb+""+"to the Mall"+myAdverb;
+console.log(wordBlanks("meow","cutie","walked","slowly"));
